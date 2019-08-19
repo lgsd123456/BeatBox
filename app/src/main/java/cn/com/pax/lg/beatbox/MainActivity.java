2248689078346600c01488ed954c925f75947e81
lgsd123456 <hbhg8612814@hotmail.com>
2019-08-19 16:21:03 +0800

    finish 20 chapter

--- app/src/main/java/cn/com/pax/lg/beatbox/MainActivity.java
+++ app/src/main/java/cn/com/pax/lg/beatbox/MainActivity.java
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment;
 import android.os.Bundle;
 
 public class MainActivity extends SingleFragmentActivity {
-
     @Override
     protected Fragment createFragment() {
         return BeatBoxFragment.newInstance();
